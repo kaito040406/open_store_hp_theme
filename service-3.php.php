3398582bcae6460d8bb25aa8f7ee07ef36e56b80
Yuma Tanaka <yumat9637@gmail.com>
2021-05-19 19:56:20 +0900

    変更

--- service-3.php.php
+++ service-3.php.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: service-single3.php
+Template Name: service-3.php
 */
 ?>
 
@@ -12,7 +12,7 @@ Template Name: service-single3.php
             <div class="rs-breadcrumbs bg-3">
                 <div class="container">
                     <div class="content-part text-center pt-160 pb-160">
-                        <h1 class="breadcrumbs-title white-color mb-0">クリエイティブ制作</h1>
+                        <h1 class="breadcrumbs-title white-color mb-0">ECサイト構築/運営</h1>
                     </div>
                 </div>
             </div>
@@ -25,19 +25,22 @@ Template Name: service-single3.php
                         <div class="col-lg-8 pr-40 md-pr-15 md-mb-50">
                             <img class="mb-39" src="assets/images/services/single/service-single.jpg" alt="">
 
-                            <h2>クリエイティブ制作とは</h2>
+                            <h2>ECサイト構築/運営とは</h2>
 
                             <p class="desc mb-31">
-                            ブランドのネーミングからブランドスローガンやステートメントのコピーライティング、ブランドイメージにふさわしい商品やモデルの撮影、ブランドサイトやランディングページのWEBデザインなど、ブランドを具現化するために必要なクリエイティブを幅広く提供しています。
-                            たったひとつのロゴやパッケージだとしても、ブランドのビジョンや文脈にふさわしいあり方を目指してデザインに取り組んでいます。
-                            その一つひとつの積み重ねが、ターゲットやコミュニティとの繋がりを生み出し、
-                            対話のチャンスを広げるブランドを実現させると信じて、クライアントと共に関わる全ての仕事を成功させていきたいと願っています。
+                            実店舗を持っているがWEBでも販売したい、すでにWEBで商品を売っているが自社ショップを一新したいなど様々なご要望を伺い、
+                            お客様にあった最適なECサイト構築プランをご提案します。
+                            国外EC・国内ECの構築、運営代行をワンストップでサポートしています。
+                            ECサイト運営に必要な構築からブランディング、SEO対策、運用、分析改善、構築・カスタマイズ、オンラインEC決済、販促、マーケティング、SNS運用など幅広く対応しています。
+                            お客様の商品やオリジナルブランドを独自のオンラインショップを通じて日本中、世界中のユーザーに届けます
                             </p>
 
                             <ul class="listing-style regular2 pl-10 sm-pl-0">
-                                <li>パンフレット・カタログ制作</li>
-                                <li>ユニフォームデザイン</li>
-                                <li>コンテンツ制作</li>
+                                <li>ECサイト制作</li>
+                                <li>システムカスタマイズ</li>
+                                <li>KPI設定</li>
+                                <li>サイト運営代行</li>
+                                <li>商品撮影</li>
                             </ul>
 
                             <div class="content-part text-center pt-160 pb-160">
@@ -45,10 +48,10 @@ Template Name: service-single3.php
                             </div>
 
 
-                            <h3 style="padding-top:50px;">創造がビジネスを加速させる</h3>
+                            <h3 style="padding-top:50px;">あなたのお店を、全ての場所に</h3>
                             <p class="desc mb-53">
-                            ユーザーのココロと行動を変化させ、 お客様のブランドが
-                            人々の記憶に深く刻まれるような "伝わる"クリエイティブを制作します。
+                            お客様の商品やオリジナルブランドを独自のオンラインショップを通じて
+                            誰にでもどこにでもお届けすることができます。
                             </p>
 
                             <div class="row gutter-20">
--- service-4.php
+++ service-4.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: service-single3.php
+Template Name: service-4.php
 */
 ?>
 
