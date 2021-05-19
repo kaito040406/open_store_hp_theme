f3f973fde0dc14bb0f991a22503e73d29bb40ed4
Yuma Tanaka <yumat9637@gmail.com>
2021-05-19 20:14:41 +0900

    Update header.php

--- header.php
+++ header.php
@@ -91,12 +91,12 @@
                                                 <li class="menu-item-has-children">
                                                     <a href="/services">サービス</a>
                                                     <ul class="sub-menu">
-                                                        <li><a href="/services-single">WEBコンサルティング</a></li>
-                                                        <li><a href="/services-single2">WEBサービス制作/開発</a></li>
-                                                        <li><a href="/services-single3">ECサイト構築/運営</a></li>
-                                                        <li ><a href="/services-single4">WEBマーケティング/運用</a></li>
-                                                        <li><a href="/services-single5">クリエイティブ制作</a></li>
-                                                        <li class="last-item"><a href="/services-single6">システム開発</a></li>
+                                                        <li><a href="/service/1">WEBコンサルティング</a></li>
+                                                        <li><a href="/service/2">WEBサービス制作/開発</a></li>
+                                                        <li><a href="/service/3">ECサイト構築/運営</a></li>
+                                                        <li ><a href="/service/4">WEBマーケティング/運用</a></li>
+                                                        <li><a href="/service/5">クリエイティブ制作</a></li>
+                                                        <li class="last-item"><a href="/service/6">システム開発</a></li>
                                                     </ul>
                                                 </li>
 
