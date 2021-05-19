d145cf318a515142d5364802b3638dc7691af77b
hidetonishi <next1993.4@gmail.com>
2021-05-19 16:46:23 +0900

    header menu text change

--- header.php
+++ header.php
@@ -85,140 +85,40 @@
                                         </div>
                                         <nav class="rs-menu">
                                             <ul class="nav-menu">
-                                                <li class="rs-mega-menu mega-rs menu-item-has-children current-menu-item"> <a href="#">Home</a>
-                                                    <ul class="mega-menu"> 
-                                                        <li class="mega-menu-container">
-                                                            <div class="mega-menu-innner">
-                                                                <div class="single-megamenu">
-                                                                    <ul class="sub-menu">
-                                                                        <li class="menu-title">Home Multipage</li>
-                                                                        <li><a href="index.html">Business Main</a> </li>
-                                                                        <li><a href="index2.html">Business 3</a> </li>
-                                                                        <li><a href="index3.html">Consulting 2</a> </li>
-                                                                        <li><a href="index4.html">Consulting 1</a> </li>
-                                                                        <li><a href="index5.html">Business 2</a> </li>
-                                                                        <li><a href="index6.html">Insurance</a> </li>
-                                                                        <li><a href="index7.html">Saas</a> </li>
-                                                                        <li><a href="index8.html">Human Resource</a> </li>
-                                                                        <li><a href="index9.html">Digital Agency</a> </li>
-                                                                    </ul>
-                                                                </div>
-                                                                <div class="single-megamenu">
-                                                                    <ul class="sub-menu">
-                                                                        <li class="menu-title">Home Multipage</li>
-                                                                        <li><a href="index10.html">Law Firm</a></li>
-                                                                        <li><a href="index11.html">Consulting 3</a></li>
-                                                                        <li><a href="index12.html">Digital Agency(SEO)</a></li>
-                                                                        <li class="active"><a href="index13.html">Digital Agency</a></li>
-                                                                        <li><a href="index14.html">Corporate Business</a></li>
-                                                                        <li><a href="index15.html">App Landing</a></li>
-                                                                        <li><a href="index16.html">It Solutions</a></li>
-                                                                        <li><a href="index17.html">Marketing Agency</a></li>
-                                                                        <li class="last-item"><a href="index18.html">Corporate Business 2</a></li>
-                                                                    </ul>
-                                                                </div>
-                                                                <div class="single-megamenu">
-                                                                 <ul class="sub-menu last-sub-menu">
-                                                                     <li class="menu-title">Home Onepage</li>
-                                                                     <li><a href="onepage1.html">Onepage 1</a> </li>
-                                                                     <li><a href="onepage2.html">Onepage 2</a> </li>
-                                                                     <li><a href="onepage3.html">Onepage 3</a> </li>
-                                                                     <li><a href="onepage4.html">Onepage 4</a> </li>
-                                                                     <li><a href="onepage5.html">Onepage 5</a> </li>
-                                                                     <li><a href="onepage6.html">Onepage 6</a> </li>                        
-                                                                     <li><a href="onepage8.html">Onepage 8</a> </li>
-                                                                     <li><a href="onepage9.html">Onepage 9</a> </li>
-                                                                     <li><a href="onepage10.html">Onepage 10</a> </li>
-                                                                 </ul>
-                                                             </div>
-                                                             <div class="single-megamenu">
-                                                                 <ul class="sub-menu last-sub-menu">
-                                                                     <li class="menu-title">Home Onepage</li>
-                                                                     <li><a href="onepage11.html">Onepage 11</a> </li>
-                                                                     <li><a href="onepage12.html">Onepage 12</a> </li>
-                                                                     <li><a href="onepage13.html">Onepage 13</a> </li>
-                                                                     <li><a href="onepage14.html">Onepage 14</a> </li>
-                                                                     <li><a href="onepage15.html">Onepage 15</a> </li>
-                                                                     <li><a href="onepage16.html">Onepage 16</a> </li>
-                                                                     <li><a href="onepage17.html">Onepage 17</a> </li>
-                                                                     <li><a href="#">Coming Soon</a> </li>
-                                                                     <li class="last-item"><a href="#">Coming Soon</a> </li>
-                                                                 </ul>
-                                                             </div>
-                                                            </div>
-                                                        </li>
-                                                    </ul> <!-- //.mega-menu --> 
+                                                <li class="rs-mega-menu mega-rs menu-item-has-children current-menu-item"> <a href="#">オープンストア</a>
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Pages</a>
+                                                    <a href="#">サービス</a>
                                                     <ul class="sub-menu">
-                                                        <li><a href="/about">About Us</a></li>
-                                                        <li class="menu-item-has-children">
-                                                            <a href="#">Services</a>
-                                                            <ul class="sub-menu">
-                                                                <li><a href="services.html">Services 1</a> </li>
-                                                                <li class="last-item"><a href="services2.html">Services 2</a> </li>
-                                                            </ul>
-                                                        </li>
-                                                        <li><a href="services-single.html">Services Single</a></li>
-                                                        <li class="menu-item-has-children">
-                                                            <a href="#">Our Peoples</a>
-                                                            <ul class="sub-menu">
-                                                                <li><a href="team.html">Team Grid Style1</a> </li>
-                                                                <li class="last-item"><a href="team2.html">Team Grid Style2</a> </li>
-                                                            </ul>
-                                                        </li>
-                                                        <li><a href="price-tables.html">Price Tables</a></li>
-                                                        <li class="last-item"><a href="faq.html">Faq</a></li>
+                                                        <li><a href="/about">WEBコンサルティング</a></li>
+                                                        <li><a href="#">WEBサービス制作/開発</a></li>
+                                                        <li><a href="services-single.html">ECサイト構築/運営</a></li>
+                                                        <li ><a href="#">WEBマーケティング/運用</a></li>
+                                                        <li><a href="price-tables.html">クリエイティブ制作</a></li>
+                                                        <li class="last-item"><a href="faq.html">システム開発</a></li>
                                                     </ul>
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Portfolios</a>
+                                                    <a href="#">NEWS</a>
                                                     <ul class="sub-menu">
-                                                        <li><a href="portfolio.html">Portfolio One</a></li>
-                                                        <li><a href="portfolio2.html">Portfolio Two</a></li>
-                                                        <li><a href="portfolio3.html">Portfolio Three</a></li>
-                                                        <li><a href="portfolio4.html">Portfolio Four</a></li>
-                                                        <li><a href="portfolio5.html">Portfolio Five</a></li>
-                                                        <li><a href="portfolio6.html">Portfolio Six</a></li>
-                                                        <li class="last-item menu-item-has-children">
-                                                            <a href="#">Portfolio Single</a>
-                                                            <ul class="sub-menu">
-                                                                <li><a href="portfolio-standard.html">Standard Style</a> </li>
-                                                                <li><a href="portfolio-slider.html">Slider Style</a> </li>
-                                                                <li class="last-item"><a href="portfolio-gallery.html">Gallery Style</a> </li>
-                                                            </ul>
-                                                        </li>
+                                                        <li><a href="portfolio.html">オープンストアブログ</a></li>
+                                                        <li><a href="portfolio2.html">お知らせ(ブログ)</a></li>
+                                                        <li><a href="portfolio3.html">Q&A</a></li>
                                                     </ul>
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Blog</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="/blog">Blog</a></li>
-                                                        <li class="last-item"><a href="/blog">Blog Single</a></li>
-                                                    </ul>
+                                                    <a href="#">制作実績</a>
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Shop</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="shop.html">Shop</a></li>
-                                                        <li><a href="shop-single.html">Shop Single</a></li>
-                                                        <li><a href="cart.html">Cart</a></li>
-                                                        <li><a href="checkout.html">Checkout</a></li>
-                                                        <li class="last-item"><a href="account.html">My Account</a></li>
-                                                    </ul>
+                                                    <a href="#">採用ページ</a>
                                                 </li>
 
                                                 <li class="menu-item-has-children">
-                                                    <a href="#">Contact</a>
-                                                    <ul class="sub-menu">
-                                                        <li><a href="/contact">Contact 1</a></li>
-                                                        <li class="last-item"><a href="/contact">Contact 2</a></li>
-                                                    </ul>
+                                                    <a href="#">お問い合わせ</a>
                                                 </li>
                                             </ul> <!-- //.nav-menu -->
                                         </nav>
