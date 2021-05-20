2f50bddd2621b8e477615f98b81c5aad0a727ce3
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-05-20 14:59:48 +0900

    index.php修正
    ブログ情報取得

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
 <?php 
     //実績のデータを取得 長谷川 start
     global $post;
-    $args_works_ja = array(
+    $args_works = array(
         'post_type' => 'works', // 実績を指定
-        'posts_per_page' => 6, // 表示する記事数(3件)
+        'posts_per_page' => 6, // 表示する記事数(6件)
     );
     // クエリ発行
-    $works_posts = get_posts($args_works_ja);
+    $works_posts = get_posts($args_works);
+
+    $args_blogs = array(
+        'post_type' => 'post', // ブログを指定
+        'posts_per_page' => 6, // 表示する記事数(6件)
+    );
+    // クエリ発行
+    $blog_posts = get_posts($args_blogs);
 
     // headerとfooterに言語パラメータを渡す
     $args = [
+        'blog' => $blog_posts,
         'works' => $works_posts,
     ];
 
