a8dcb4e3d3e56ae7c54714dcd6f920053060a273
open-store-inc <keikubo.999@gmail.com>
2021-05-20 16:34:54 +0900

    画像埋め込み

--- templates/index_body.php
+++ templates/index_body.php
@@ -119,34 +119,34 @@
                     <div class="container">
                         <div class="rs-carousel owl-carousel" data-loop="true" data-items="5" data-margin="30" data-autoplay="true" data-hoverpause="true" data-autoplay-timeout="5000" data-smart-speed="800" data-dots="false" data-nav="false" data-nav-speed="false" data-center-mode="false" data-mobile-device="1" data-mobile-device-nav="false" data-mobile-device-dots="false" data-ipad-device="3" data-ipad-device-nav="false" data-ipad-device-dots="false" data-ipad-device2="2" data-ipad-device-nav2="false" data-ipad-device-dots2="false" data-md-device="5"  data-lg-device="5" data-md-device-nav="false" data-md-device-dots="false">
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/1.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/shopify_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/2.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/google_dw_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/3.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/shopify_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/4.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/google_dw_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/5.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/shopify_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/1.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/google_dw_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/2.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/shopify_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/3.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/google_dw_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/4.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/shopify_logo.png" alt="">
                             </div>
                             <div class="partner-item">
-                                <a href="https://rstheme.com"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/partner/gray2/5.png" alt=""></a>
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/nishi/google_dw_logo.png" alt="">
                             </div>
                         </div>
                     </div>
