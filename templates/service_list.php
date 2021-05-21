f050078aafe88953919940d452699b6aa101a00a
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-05-21 17:33:31 +0900

    テンプレートか呼び出し

--- templates/service_list.php
+++ templates/service_list.php
@@ -1,75 +1,3 @@
-<?php
-/*
-Template Name: service-3.php
-*/
-?>
-
-<?php get_header(); ?>
-
-		<!-- Main content Start -->
-        <div class="main-content">
-            <!-- Breadcrumbs Section Start -->
-            <div class="rs-breadcrumbs bg-3">
-                <div class="container">
-                    <div class="content-part text-center pt-160 pb-160">
-                        <h1 class="breadcrumbs-title title-color2 mb-0">ECサイト構築/運営</h1>
-                    </div>
-                </div>
-            </div>
-            <!-- Breadcrumbs Section End -->
-
-            <!-- Services Section Start -->
-            <div id="rs-services" class="rs-services single pt-100 pb-100 md-pt-80 md-pb-80">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-8 pr-40 md-pr-15 md-mb-50">
-                            <img class="mb-39" src="assets/images/services/single/service-single.jpg" alt="">
-
-                            <h2>ECサイト構築/運営とは</h2>
-
-                            <p class="desc mb-31">
-                            実店舗を持っているがWEBでも販売したい、すでにWEBで商品を売っているが自社ショップを一新したいなど様々なご要望を伺い、
-                            お客様にあった最適なECサイト構築プランをご提案します。
-                            国外EC・国内ECの構築、運営代行をワンストップでサポートしています。
-                            ECサイト運営に必要な構築からブランディング、SEO対策、運用、分析改善、構築・カスタマイズ、オンラインEC決済、販促、マーケティング、SNS運用など幅広く対応しています。
-                            お客様の商品やオリジナルブランドを独自のオンラインショップを通じて日本中、世界中のユーザーに届けます
-                            </p>
-
-                            <ul class="listing-style regular2 pl-10 sm-pl-0">
-                                <li>ECサイト制作</li>
-                                <li>システムカスタマイズ</li>
-                                <li>KPI設定</li>
-                                <li>サイト運営代行</li>
-                                <li>商品撮影</li>
-                            </ul>
-
-                            <div class="content-part text-center pt-160 pb-160">
-                                <h1 class="breadcrumbs-title white-color mb-0">WEBコンサルティング</h1>
-                            </div>
-
-
-                            <h3 style="padding-top:50px;">あなたのお店を、全ての場所に</h3>
-                            <p class="desc mb-53">
-                            お客様の商品やオリジナルブランドを独自のオンラインショップを通じて
-                            誰にでもどこにでもお届けすることができます。
-                            </p>
-
-                            <div class="row gutter-20">
-                                <div class="col-6">
-                                    <img class="bdru-4" src="assets/images/services/single/1.jpg" alt="">
-                                </div>
-                                <div class="col-6">
-                                    <img class="bdru-4" src="assets/images/services/single/2.jpg" alt="">
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- Services Section End -->
-        </div> 
-        <!-- Main content End -->
                     <!-- Services Section Start -->
                     <div id="rs-services" class="rs-services style16 bg29">
                 <div class="container">
@@ -84,7 +12,7 @@ Template Name: service-3.php
                                     <i class="fa fa-bar-chart"></i>
                                 </div>
                                 <div class="services-txt">
-                                    <h4 class="services-title"><a href="/services">WEBコンサルティング</a></h4>
+                                    <h4 class="services-title"><a href="/services/consulting">WEBコンサルティング</a></h4>
                                     <p class="services-txt">綿密なヒアリングをもとにWEBサービスの問題発見や解決策の提案、戦略立案などを中心に、お客様が最適なWEBサービスを活用できるサポートします。</p>
                                     <div class="serial-number"> ~~~ 01</div>
                                 </div>
@@ -96,7 +24,7 @@ Template Name: service-3.php
                                     <i class="fa fa-puzzle-piece"></i>
                                 </div>
                                 <div class="services-txt">
-                                    <h4 class="services-title"><a href="/services">WEBサービス制作/開発</a></h4>
+                                    <h4 class="services-title"><a href="/service/webservice">WEBサービス制作/開発</a></h4>
                                     <p class="services-txt">お客様の「伝えたいこと」「やりたいこと」とユーザーの求めていることをWEBサービスを用いてさまざまなカタチで実現します。</p>
                                     <div class="serial-number"> ~~~ 02</div>
                                 </div>
@@ -108,7 +36,7 @@ Template Name: service-3.php
                                     <i class="fa fa-envelope-o"></i>
                                 </div>
                                 <div class="services-txt">
-                                    <h4 class="services-title"><a href="/services">ECサイト構築/運営</a></h4>
+                                    <h4 class="services-title"><a href="/service/ecservice">ECサイト構築/運営</a></h4>
                                     <p class="services-txt">お客様の商品やオリジナルブランドを独自のオンラインショップを通じて誰にでもどこにでもお届けすることができます。</p>
                                     <div class="serial-number"> ~~~ 03</div>
                                 </div>
@@ -120,7 +48,7 @@ Template Name: service-3.php
                                     <i class="fa fa-sitemap"></i>
                                 </div>
                                 <div class="services-txt">
-                                    <h4 class="services-title"><a href="/services">WEBマーケティング/運用</a></h4>
+                                    <h4 class="services-title"><a href="/service/webmarketing">WEBマーケティング/運用</a></h4>
                                     <p class="services-txt">デジタル広告の重要性が非常に高まっている今、インターネット上でユーザーを集める為の仕組みづくりを提供します。</p>
                                     <div class="serial-number"> ~~~ 04</div>
                                 </div>
@@ -132,7 +60,7 @@ Template Name: service-3.php
                                     <i class="fa fa-codepen"></i>
                                 </div>
                                 <div class="services-txt">
-                                    <h4 class="services-title"><a href="/services">クリエイティブ制作</a></h4>
+                                    <h4 class="services-title"><a href="/service/creative">クリエイティブ制作</a></h4>
                                     <p class="services-txt">ユーザーの心と行動を変化させ、お客様のブランドが人々の記憶に深く刻まれるような"伝わる"クリエイティブ制作をします。</p>
                                     <div class="serial-number"> ~~~ 05</div>
                                 </div>
@@ -144,7 +72,7 @@ Template Name: service-3.php
                                     <i class="fa fa-yelp"></i>
                                 </div>
                                 <div class="services-txt">
-                                    <h4 class="services-title"><a href="/services">システム開発</a></h4>
+                                    <h4 class="services-title"><a href="/service/system">システム開発</a></h4>
                                     <p class="services-txt">顧客管理、営業管理システムといったITの導入により業務を効率化し新たなビジネスチャンスを創ります。</p>
                                     <div class="serial-number"> ~~~ 06</div>
                                 </div>
@@ -154,6 +82,3 @@ Template Name: service-3.php
                 </div>
             </div>
             <!-- Services Section End -->
-
-        <!-- Footer Start -->
-        <?php get_footer(); ?>
