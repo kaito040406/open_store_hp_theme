a3c8c572d3fef9d300bd6e47bc442086c8877fe4
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-05-21 14:02:51 +0900

    修正

---
+++ privacy.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Template Name: privacy.php
+*/
+?>
+
+<?php get_header(); ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/about.css">
+<!-- Main content Start -->
+<div class="main-content">
+    <!-- Breadcrumbs Section Start -->
+    <div class="rs-breadcrumbs bg-3">
+        <div class="container">
+            <div class="content-part text-center pt-160 pb-160">
+                <h1 class="breadcrumbs-title title-color2 mb-0">会社情報</h1>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumbs Section End -->
+
+    <!-- Services Section Start -->
+    <div class="container">
+        <div class="kaisha_bun">
+            <div class="kaisha_bun_title">
+                <h2>プライバシーポリシー</h2>
+            </div>
+            <div class="kaisha_bun_me">
+                <p>
+ここに入れる
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- Main content End -->
+
+    <!-- Services Section Start -->
+    <div id="rs-services" class="rs-services style16 bg29">
+        <div class="container">
+            <div class="sec-title3 text-center mb-45 md-mb-30">
+                <p class="title title3">ビジネス課題を解決しています。</p>
+                <span class="sub-title">~ <span class="title-upper">まずは課題・お悩みをお聞かせください。</span> ~</span>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-25">
+                    <div class="services-wrap">
+                        <div class="icon-part">
+                            <i class="fa fa-bar-chart"></i>
+                        </div>
+                        <div class="services-txt">
+                            <h4 class="services-title"><a href="<?php bloginfo('url'); ?>/service/consulting">WEBコンサルティング</a></h4>
+                            <p class="services-txt">綿密なヒアリングをもとにWEBサービスの問題発見や解決策の提案、戦略立案などを中心に、お客様が最適なWEBサービスを活用できるサポートします。</p>
+                            <div class="serial-number"> ~~~ 01</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-25">
+                    <div class="services-wrap">
+                        <div class="icon-part">
+                            <i class="fa fa-puzzle-piece"></i>
+                        </div>
+                        <div class="services-txt">
+                            <h4 class="services-title"><a href="<?php bloginfo('url'); ?>/service/webservice">WEBサービス制作/開発</a></h4>
+                            <p class="services-txt">お客様の「伝えたいこと」「やりたいこと」とユーザーの求めていることをWEBサービスを用いてさまざまなカタチで実現します。</p>
+                            <div class="serial-number"> ~~~ 02</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 mb-25">
+                    <div class="services-wrap">
+                        <div class="icon-part">
+                            <i class="fa fa-envelope-o"></i>
+                        </div>
+                        <div class="services-txt">
+                            <h4 class="services-title"><a href="<?php bloginfo('url'); ?>/service/ecservice">ECサイト構築/運営</a></h4>
+                            <p class="services-txt">お客様の商品やオリジナルブランドを独自のオンラインショップを通じて誰にでもどこにでもお届けすることができます。</p>
+                            <div class="serial-number"> ~~~ 03</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 md-mb-25">
+                    <div class="services-wrap">
+                        <div class="icon-part">
+                            <i class="fa fa-sitemap"></i>
+                        </div>
+                        <div class="services-txt">
+                            <h4 class="services-title"><a href="<?php bloginfo('url'); ?>/service/webmarketing">WEBマーケティング/運用</a></h4>
+                            <p class="services-txt">デジタル広告の重要性が非常に高まっている今、インターネット上でユーザーを集める為の仕組みづくりを提供します。</p>
+                            <div class="serial-number"> ~~~ 04</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 sm-mb-25">
+                    <div class="services-wrap">
+                        <div class="icon-part">
+                            <i class="fa fa-codepen"></i>
+                        </div>
+                        <div class="services-txt">
+                            <h4 class="services-title"><a href="<?php bloginfo('url'); ?>/service/creative">クリエイティブ制作</a></h4>
+                            <p class="services-txt">ユーザーの心と行動を変化させ、お客様のブランドが人々の記憶に深く刻まれるような"伝わる"クリエイティブ制作をします。</p>
+                            <div class="serial-number"> ~~~ 05</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <div class="services-wrap">
+                        <div class="icon-part">
+                            <i class="fa fa-yelp"></i>
+                        </div>
+                        <div class="services-txt">
+                            <h4 class="services-title"><a href="<?php bloginfo('url'); ?>/service/system">システム開発</a></h4>
+                            <p class="services-txt">顧客管理、営業管理システムといったITの導入により業務を効率化し新たなビジネスチャンスを創ります。</p>
+                            <div class="serial-number"> ~~~ 06</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Services Section End -->
+
+    <!-- Footer Start -->
+    <?php get_footer(); ?>
