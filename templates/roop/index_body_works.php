1c826076ea9ffd98336c1769120b2068120c9832
kaito hasegawa <kaitohasegawa000@gmail.com>
2021-05-21 16:08:48 +0900

    リンク先変更

--- templates/roop/index_body_works.php
+++ templates/roop/index_body_works.php
@@ -3,8 +3,8 @@
       <a href="<?php echo get_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
   </div>
   <div class="project-inner">
-      <h4 class="title"><a href="portfolio-slider.html"><?php echo the_title(); ?></a></h4>
+      <h4 class="title"><a href="<?php echo get_permalink(); ?>"><?php echo the_title(); ?></a></h4>
       <?php //ここはなんのサービスに該当するかを書く。あとでよい ?>
-      <span class="category"><a href="portfolio-slider.html">Digital Agency</a></span>
+      <span class="category"><a href="<?php echo get_permalink(); ?>">詳細</a></span>
   </div>
 </div>
