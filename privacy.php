4137d7d021e4d499d298bd91a6e14669e2822cc8
ookuraseiya <79490150+ookuraseiya@users.noreply.github.com>
2021-05-21 14:39:17 +0900

    プライバシーポリシー　作成

--- privacy.php
+++ privacy.php
@@ -5,14 +5,14 @@ Template Name: privacy.php
 ?>
 
 <?php get_header(); ?>
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/about.css">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/privacy.css">
 <!-- Main content Start -->
 <div class="main-content">
     <!-- Breadcrumbs Section Start -->
     <div class="rs-breadcrumbs bg-3">
         <div class="container">
             <div class="content-part text-center pt-160 pb-160">
-                <h1 class="breadcrumbs-title title-color2 mb-0">会社情報</h1>
+                <h1 class="breadcrumbs-title title-color2 mb-0">プライバシーポリシー</h1>
             </div>
         </div>
     </div>
@@ -20,14 +20,82 @@ Template Name: privacy.php
 
     <!-- Services Section Start -->
     <div class="container">
-        <div class="kaisha_bun">
-            <div class="kaisha_bun_title">
-                <h2>プライバシーポリシー</h2>
-            </div>
-            <div class="kaisha_bun_me">
-                <p>
-ここに入れる
-                </p>
+        <div class="privacy_policy">
+            <div class="privacy_policy_1">
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>個人情報保護方針</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>オープンストア株式会社（以下「当社」）は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。また、入力された個人情報をプライバシーポリシー利用目的の範囲内でのみ利用させていただきます。
+                        </p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>個人情報の管理</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>当社は、お客さまの個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。
+                        </p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>個人情報の利用目的</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>お客さまからお預かりした個人情報は、当社からのご連絡や業務のご案内やご質問に対する回答として、電子メールや資料のご送付に利用いたします。</p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>個人情報の第三者への開示・提供の禁止</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>当社は、お客さまよりお預かりした個人情報を適切に管理し、次のいずれかに該当する場合を除き、個人情報を第三者に開示いたしません。</p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>お客さまの同意がある場合</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>お客さまが希望されるサービスを行なうために当社が業務を委託する業者に対して開示する場合
+                            法令に基づき開示することが必要である場合
+                            個人情報の安全対策。
+                            当社は、個人情報の正確性及び安全性確保のために、セキュリティに万全の対策を講じています。</p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>ご本人の照会</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>お客さまがご本人の個人情報の照会・修正・削除などをご希望される場合には、ご本人であることを確認の上、対応させていただきます。</p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>ポリシーの変更と見直し</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。変更後の本ポリシーについては、当サイトに掲載した時から効力を生じるものと致します。
+                        </p>
+                    </div>
+                </div>
+                <div class="policy">
+                    <div class="policy_title">
+                        <h3>お問い合せ</h3>
+                    </div>
+                    <div class="policy_me">
+                        <p>当社の個人情報の取扱に関するお問い合せは下記までご連絡ください。</p>
+                        <p>オープンストア株式会社</p>
+                        <p>大阪府大阪市淀川区木川東4-5-3 オパル新大阪ビル
+                        </p>
+                        <p>TEL/FAX (06)6770-9626</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
